d688246895433cdc92925239c907838a36442be9
Ericsson Budhilaw <budhilaw@Ericssons-MacBook-Air.local>
2021-06-13 23:07:43 +0700

    Progress 4: Onprogress Landing Page, Shop Page dan Contact us

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,11 +10,24 @@
         @livewireStyles
     </head>
     <body class="antialiased">
-        <p>Coming soon.</p>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Button
-        </button>
-{{--        <img src="{{ asset('storage/Gigabyte-RX5600XT.png') }}" alt="">--}}
+        <livewire:navbar />
+        <section class="hero">
+            <div class="mx-auto py-5 container grid gap-4 grid-cols-2">
+                <div class="mx-auto">
+                    <img src="{{ asset("storage/RTX3090-ASUS.png") }}" alt="ASUS ROG RTX 3090" width="555" height="485" />
+                </div>
+                <div class="pt-24 antialiased font-sans">
+                    <p class="mb-8 text-4xl font-bold text-gray-900">ASUS ROG RTX 3090</p>
+                    <p class="text-2xl font-bold tracking-wide text-gray-700">Performa terbaik, suhu yang terjaga</p>
+                    <p class="mb-8 text-2xl font-bold tracking-wide text-gray-700">Semua permainan dapat ditaklukan</p>
+                    <div class="price-tag grid gap-2 grid-cols-2 w-1/2">
+                        <p class="text-1xl text-center p-4 bg-blue-600 text-gray-300 shadow">Rp.16.999.000</p>
+                        <button class="text-1xl text-center tracking-wider bg-gray-300 shadow">BELI SEKARANG</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+{{--        <img src="{{ asset('storage/toko-togo.png') }}" alt="">--}}
 
         <livewire:counter />
 
